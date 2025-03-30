aaa788baf888f34f5332f629859034e4208c98e3
Victor Ohashi <victor.ohashi@govoll.com>
2025-03-30 14:06:02 -0300

    fix: Enhance prediction response structure

--- api/main.py
+++ api/main.py
@@ -1,29 +1,40 @@
 import os
 import pickle
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
+from pathlib import Path
 
 app = FastAPI()
 
-MODEL_DIR = "../build/models"
-with open(os.path.join(MODEL_DIR, "svc_model.pkl"), "rb") as f:
+MODEL_DIR = Path(os.path.dirname(__file__)) / "../build/models"
+
+svc_model_path = MODEL_DIR / "svc_model.pkl"
+print(f"Loading SVC model from {svc_model_path.resolve()}")
+with svc_model_path.open("rb") as f:
     svc_engine = pickle.load(f)
-with open(os.path.join(MODEL_DIR, "knn_model.pkl"), "rb") as f:
+
+knn_model_path = MODEL_DIR / "knn_model.pkl"
+print(f"Loading KNN model from {knn_model_path.resolve()}")
+with knn_model_path.open("rb") as f:
     knn_engine = pickle.load(f)
 
 class QueryRequest(BaseModel):
     query: str
 
+class RoutePrediction(BaseModel):
+    id: str
+    path: str
+
 @app.post("/predict/svc")
 def predict_svc(request: QueryRequest):
     predictions = svc_engine.predict(request.query)
-    return {"predictions": [route.dict() for route in predictions]}
+    return {"predictions": [RoutePrediction(id=route.id, path=route.path) for route in predictions]}
 
 @app.post("/predict/knn")
 def predict_knn(request: QueryRequest):
     predictions = knn_engine.predict(request.query)
-    return {"predictions": [route.dict() for route in predictions]}
+    return {"predictions": [RoutePrediction(id=route.id, path=route.path) for route in predictions]}
 
 def run():
     """Launched with `poetry run start` at root level"""
